399b7f55f77073d4d1e8d051a37496343eeed08b
Ron Zapata <rn23a***a@gmail.com>
2026-04-03 23:59:15 +0800

    Edited signup.html and index.html to use more vanilla CSS, added classes in styles.css and renamed HTML-BUYER to HTML-DEFAULT

--- JS/signup.js
+++ JS/signup.js
@@ -1,43 +1,13 @@
-//This is the original version of signup.js before overhaul
+let currentPage = 0;
 
-function nextPage(currentId) {
-    const currentSection = document.getElementById(currentId);
-    const role = document.getElementById('signup-user-acctype').value;
-    const roleTracker = document.getElementById('user-role-tracker');
-    
-    // Update the hidden variable to keep track for the final redirect
-    roleTracker.value = role;
+showPage(currentPage);
 
-    // Hide current section
-    currentSection.classList.add('hidden');
+function showPage(n) {
+    //getelemclassname creates an array apparently
+    let pages = document.getElementsByClassName("localfunc-signup-page");
+    pages[n].style.display = "block";
 
-    // Logic for branching paths
-    if (currentId === 'pg-1') {
-        if (role === 'buyer-acc') {
-            document.getElementById('signup-pg-2-buyer').classList.remove('hidden');
-        } else {
-            document.getElementById('signup-pg-2-seller').classList.remove('hidden');
-        }
-    } 
-    // Buyer Flow Navigation
-    else if (currentId === 'signup-pg-2-buyer') {
-        document.getElementById('signup-pg-3-buyer').classList.remove('hidden');
-    } 
-    else if (currentId === 'signup-pg-3-buyer') {
-        document.getElementById('signup-pg-4-buyer').classList.remove('hidden');
-    }
-    // Final Transitions to Success Page
-    else if (currentId === 'signup-pg-4-buyer' || currentId === 'signup-pg-2-seller') {
-        document.getElementById('signup-user-final_page').classList.remove('hidden');
-    }
-}
-
-function finalRedirect() {
-    const role = document.getElementById('user-role-tracker').value;
-    
-    if (role === 'seller-acc') {
-        window.location.href = "seller.html";
-    } else {
-        window.location.href = "products.html";
+    if (n === 0) {
+        
     }
 }
